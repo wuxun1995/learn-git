26e41e9054f1bd3b380f20663cc422f32cb3231a
wuxun1995 <1807941261@qq.com>
2022-01-10 16:15:10 +0800

    kl

--- lodash/wuxun1995-lodash.js
+++ lodash/wuxun1995-lodash.js
@@ -1,20 +1,20 @@
-<script>
-  var wuxun1995 = {
-    chunk: function (array, size) {
-      var array = []
-      var newarray = []
-      for (var i = 1; i <= array.length; i++) {
-        newarray.push(array[i - 1])
-        if (i % size == 0) {
-          array.push(newarray)
-          newarray.push = []
-        }
-      }
-      if (array.length) {
-        array.push(newarray)
-      }
-      return array
-  }
 
+var wuxun1995 = {
+  chunk: function (array, size) {
+    var array = []
+    var newarray = []
+    for (var i = 1; i <= array.length; i++) {
+      newarray.push(array[i - 1])
+      if (i % size == 0) {
+        array.push(newarray)
+        newarray.push = []
+      }
+    }
+    if (array.length) {
+      array.push(newarray)
+    }
+    return array
+  }
 }
-</script>
+
+
