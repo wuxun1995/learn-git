3ab74aad73cdabcd4fed553328067d2e6c2de1b3
wuxun1995 <1807941261@qq.com>
2022-01-10 22:51:42 +0800

    po

--- lodash/wuxun1995-lodash.js
+++ lodash/wuxun1995-lodash.js
@@ -16,5 +16,69 @@ var wuxun1995 = {
       newarray.push(array1)
     }
     return newarray
+  },
+
+  compact: function (array) {
+    let newArray = []
+    for (i = 0; i < array.length; i++) {
+      if (array[i] == false || array[i] == null || array[i] == 0 || array[i] == "" || array[i] == undefined || array[i] == NaN) {
+        newArray.push()
+      } else {
+        newArray.push(array[i])
+      }
+    }
+    return newArray
+  },
+
+  difference: function (array, [values]) {
+    for (i = 0; i < array.length; i++) {
+      result = []
+      for (j = 0; j < [values].length; j++) {
+        if (array[i] !== [values][j]) {
+          result.push(array[i])
+        }
+      }
+    }
+    return result
+  },
+
+  differenceBy: function (array, [values], [iteratee = _.identity]) {
+    arr = [iteratee = _.identity](array)
+    val = [iteratee = _.identity]([values])
+    for (i = 0; i < array.length; i++) {
+      result = []
+      for (j = 0; j < [values].length; j++) {
+        if (arr[i] !== val[j]) {
+          result.push(array[i])
+        }
+      }
+    }
+    return result
+  },
+
+  drop: function (array, n = 1) {
+    let arr = []
+    if (n >= array.length) {
+      return arr
+    }
+    for (let i = n; i < array.length; i++) {
+      arr.push(array[i])
+    }
+    return arr
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
