5b511d83f92ab18b5507e51ca947bc5e9793c9a7
wuxun1995 <1807941261@qq.com>
2022-01-10 21:11:54 +0800

    oo

--- lodash/wuxun1995-lodash.js
+++ lodash/wuxun1995-lodash.js
@@ -1,12 +1,20 @@
 var wuxun1995 = {
   chunk: function (array, size) {
     let newarray = []
+    let n = 0
+    let array1 = []
     for (var i = 0; i < array.length; i++) {
-      let array1 = []
-      for (var j = 0; j < size; j++) {
-        array1.push(array[i])
+      array1.push(array[i])
+      n++
+      if (n == size) {
+        n = 0
+        newarray.push(array1)
+        array1 = []
       }
+    }
+    if (n !== 0) {
       newarray.push(array1)
     }
     return newarray
+  }
 }
